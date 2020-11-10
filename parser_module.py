a198b66039c9e262077446bdb4ba4be414910b7b
Yuval Mor Yosef <yuvalmoryosef3@gmail.com>
2020-11-10 19:38:43 +0200

    hashtag and url

--- parser_module.py
+++ parser_module.py
@@ -2,6 +2,16 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from document import Document
 from nltk.tokenize import WhitespaceTokenizer
+import re
+
+
+
+#return pattern.findall(tag)
+
+
+# def split_hashtag(word):
+#     expanded = " ".join([word for a in re.split('([A-Z][a-z]+)') if a])
+#     return expanded
 
 
 class Parse:
@@ -17,6 +27,17 @@ class Parse:
         """
         #text_tokens = word_tokenize(text)
         text_tokens = WhitespaceTokenizer().tokenize(text)
+        # hashtags
+        split_hash = self.find_hashtags(text_tokens)
+        # url
+        split_url = self.find_url(text_tokens)
+        # remove origin urls:
+        text_tokens = self.remove_url_from_token(text_tokens)
+        # extend
+        text_tokens.extend(split_hash)
+
+        text_tokens.extend(split_url)
+        # lower&stopwords
         text_tokens_without_stopwords = [w.lower() for w in text_tokens if w not in self.stop_words]
         return text_tokens_without_stopwords
 
@@ -48,3 +69,34 @@ class Parse:
         document = Document(tweet_id, tweet_date, full_text, url, retweet_text, retweet_url, quote_text,
                             quote_url, term_dict, doc_length)
         return document
+
+    def split_hashtag(self, tag):
+        pattern = re.compile(r"[A-Z][a-z]+|\d+|[A-Z]+(?![a-z])")
+        return pattern.findall(tag)
+
+    def find_hashtags(self, text_tokens):
+        hashtags = [s for s in text_tokens if "#" in s]
+        split_hashtags = []
+        # word = []
+        for h in hashtags:
+            # word = self.split_hashtag(h)
+            split_hashtags.extend(self.split_hashtag(h))
+        return split_hashtags
+
+    def split_url(self, tag):
+        # pattern = re.compile(r'[\:/?=\-&]+',re.UNICODE)
+        # return pattern.findall(tag)
+        return re.compile(r'[\:/?=\-&]+', re.UNICODE).split(tag)
+
+    def find_url(self, text_tokens):
+        url = [s for s in text_tokens if "http" in s]
+        split_urls = []
+        # word = []
+        for h in url:
+            # word = self.split_hashtag(h)
+            split_urls.extend(self.split_url(h))
+        return split_urls
+
+    def remove_url_from_token(self, text_tokens):
+        text_tokens = [x for x in text_tokens if "http" not in x]
+        return text_tokens
